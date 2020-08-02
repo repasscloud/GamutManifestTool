3f2538620e13dd29dbffcf928b7333f2a395eaee
Danijel-James Wynyard <danijel.james@me.com>
2020-08-02 17:28:36 +1000

    appveyor testing 2020-08-02_17-28-36

--- app/Public/Test-GamutManifest.ps1
+++ app/Public/Test-GamutManifest.ps1
@@ -193,23 +193,3 @@ function Test-GamutManifest {
         [System.GC]::Collect()
     }
 }
-
-
-Test-GamutManifest -FilePath .\app\Mozilla\Firefox\latest.json
-
-
-
-<#
-for($I = 1; $I -lt 101; $I++ )
-{
-    Write-Progress -Activity Updating -Status 'Progress->' -PercentComplete $I -CurrentOperation OuterLoop
-    for($j = 1; $j -lt 101; $j++ )
-    {
-        Write-Progress -Id 1 -Activity Updating -Status 'Progress' -PercentComplete $j -CurrentOperation InnerLoop
-        Start-Sleep -Milliseconds 50
-    }
-}
-#>
-
-#Write-Color '   [',$([char]215),']' -Color Cyan,Red,Cyan
-#Write-Color '   [',$([char]8730),']' -Color Cyan,Green,Cyan
