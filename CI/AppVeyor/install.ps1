b24b7966b2874ba0c7f72e721e8822e70f18e0d0
Danijel-James Wynyard <danijel.james@me.com>
2020-08-02 23:46:14 +1000

    clean up install file

--- CI/AppVeyor/install.ps1
+++ CI/AppVeyor/install.ps1
@@ -1,10 +1,9 @@
 param ($fullPath)
-#$fullPath = 'C:\Program Files\WindowsPowerShell\Modules\GamutManifestTool'
 if (-not $fullPath) {
     $fullpath = $env:PSModulePath -split ":(?!\\)|;|," |
         Where-Object {$_ -notlike ([System.Environment]::GetFolderPath("UserProfile")+"*") -and $_ -notlike "$pshome*"} |
             Select-Object -First 1
-            $fullPath = Join-Path $fullPath -ChildPath "GamutManifestTool"
+            $fullPath = Join-Path $fullPath -ChildPath $Env:APPVEYOR_PROJECT_NAME
 }
 $srcPath = $Env:APPVEYOR_BUILD_FOLDER + '\app'
 Push-location $PSScriptRoot
